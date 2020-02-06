36ce3f052b594994006a8b34e3bd00dd25521607
marioandrecarvalho <mario.andre.franco@gmail.com>
2020-02-06 10:16:21 -0300

    adding styles and pagination

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -14,6 +14,50 @@ export default class Main extends Component{
     }
 
     loadEvent = async (page = 1) => {
+        const response = await api.get(`/nextEvent?page=${page}`)
+        const {docs, ...eventInfo} = response.data
+        this.setState({event: docs, eventInfo, page})
+    }
+
+    prevPage = () => {
+        const {page, eventInfo} = this.state
+
+        if (page === 1 ) return
+
+        const pageNumber = page - 1
+
+        this.loadEvent(pageNumber)
+    }
+
+    nextPage = () => {
+        const {page, eventInfo} = this.state
+
+        if (page === eventInfo.page) return
+
+        const pageNumber = page + 1
+
+        this.loadEvent(pageNumber)
+    }
+
+    render() {
+        const { event, page, eventInfo } = this.state
+        return (
+            <div className="event-list">
+                {event.map(event => (
+                    <article key={event._id}>
+                        <strong> {event.title}</strong>
+                        <p> {event.description}</p>
+
+                        <Link to={`/nextEvent/${event._id}`}>Acessar</Link>
+                    </article>
+                ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === eventInfo.page} onClick={this.nextPage}>Proximo</button>
+                </div>
+
+            </div>
+        )
 
     }
 }
